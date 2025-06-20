a44d80b2d4c6ae9174cbff660e62172736e6fafd
ananya-marjia <ananyamarjia@outlook.com>
2025-06-20 01:37:37 -0400

    Update init-config.ps1

--- init-config.ps1
+++ init-config.ps1
@@ -1,2 +1,6 @@
 $path = "$env:ProgramData\CloudInitFlag.txt"
 'Cloud-init executed' | Out-File -FilePath $path -Force
+
+Start-Transcript -Path "$env:ProgramData\cloudinit-log.txt" -Force
+'Cloud-init executed' | Out-File -FilePath "$env:ProgramData\CloudInitFlag.txt" -Force
+Stop-Transcript
